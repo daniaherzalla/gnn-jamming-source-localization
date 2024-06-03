0147539db3035790850cc0463b01bf18f6c918ea
dania-tii <dania.herzalla@tii.ae>
2024-06-03 10:43:27 +0400

    Update to include 9 node features and check for model weights

--- model.py
+++ model.py
@@ -2,10 +2,10 @@ import torch
 from torch_geometric.graphgym import init_weights
 from torch_geometric.nn import global_mean_pool, MLP, GCN, GraphSAGE, GIN, GAT, AttentionalAggregation
 
-from torch.nn import Linear, ReLU
-from utils import set_random_seeds
+from torch.nn import Linear
+from utils import set_seeds_and_reproducibility
 
-set_random_seeds()
+set_seeds_and_reproducibility()
 
 
 class GNN(torch.nn.Module):
@@ -17,7 +17,7 @@ class GNN(torch.nn.Module):
         num_heads (int): The number of attention heads in the GAT layers.
         in_channels (int): Input features dimension: drone pos (x,y,z), RSSI, jamming status, distance to centroid.
     """
-    def __init__(self, dropout_rate, num_heads, model_type='GAT', in_channels=6, hidden_channels=32, out_channels=64, num_layers=2, out_features=3, act=None, norm=None, v2=False):
+    def __init__(self, dropout_rate, num_heads, model_type='GAT', in_channels=9, hidden_channels=32, out_channels=64, num_layers=2, out_features=3, act='relu', norm=None):
         super(GNN, self).__init__()
 
         # Model definitions
@@ -30,15 +30,15 @@ class GNN(torch.nn.Module):
         elif model_type == 'GIN':
             self.gnn = GIN(in_channels=in_channels, hidden_channels=hidden_channels, out_channels=out_channels, num_layers=num_layers, dropout=0.0, act=act, norm=norm)
         elif model_type in ['GAT', 'GATv2']:
-            self.gnn = GAT(in_channels=in_channels, hidden_channels=hidden_channels, out_channels=out_channels, num_layers=num_layers, dropout=dropout_rate, act=act, norm=norm, heads=num_heads, v2='v2' in model_type)
+            self.gnn = GAT(in_channels=in_channels, hidden_channels=hidden_channels, out_channels=out_channels, num_layers=num_layers, dropout=0.0, act=act, norm=norm, heads=num_heads, v2='v2' in model_type)
 
         # Final layer
-        self.attention_pool = AttentionalAggregation(gate_nn=Linear(hidden_channels * num_heads, 1))
-        self.regressor = Linear(hidden_channels * num_heads, out_features)
+        self.attention_pool = AttentionalAggregation(gate_nn=Linear(out_channels, 1))
+        self.regressor = Linear(out_channels, out_features)
         self.dropout = torch.nn.Dropout(dropout_rate)
 
         # Initialize weights
-        init_weights(self)  # TODO: create custom function for gcn/other layers
+        init_weights(self)
 
     def forward(self, data):
         """
@@ -59,3 +59,10 @@ class GNN(torch.nn.Module):
         x = self.dropout(x)  # apply dropout last layer
         x = self.regressor(x)  # Predict the jammer's coordinates
         return x
+
+    def print_weights(self):
+        """
+        Print the weights of the model.
+        """
+        for name, param in self.named_parameters():
+            print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
