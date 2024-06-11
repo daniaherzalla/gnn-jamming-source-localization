201b4864118ba56092d69c53a130f381d86bbf67
dania-tii <dania.herzalla@tii.ae>
2024-06-11 11:37:51 +0400

    Add opt for zscore or minmax normalization

--- config.py
+++ config.py
@@ -21,16 +21,17 @@ params = {
     'weight_decay': 0.0001,
     'batch_size': 16,
     'dropout_rate': 0.6,
-    'num_heads': 8,  # num of heads for first two layers
+    'num_heads': 8,
     'max_epochs': 200,
     'seed': 42,  # opts: 42, 1, 23
     'feats': 'polar',  # opts: 'polar', 'cartesian', 'polar_cartesian'
-    'in_channels': 4,  # drone pos (x, y, z) + rssi
+    'in_channels': 4,  # drone pos (x, y, z) + rssi # 6 (r, sin(theta), cos(theta), sin(phi), cos(phi)) + rssi
     'num_layers': 4,
     'hidden_channels': 256,
     'out_channels': 64,
-    'out_features': 3,
+    'out_features': 3,  # 3 jammer pos (x, y, z) # 5 (r, sin(theta), cos(theta), sin(phi), cos(phi))
     'edges': 'knn',  # opts: 'knn', 'proximity'
+    'norm': 'minmax',  # opts: 'zscore', 'minmax'
     'dataset_path': 'data/static_swarm_3d.csv',
     'train_path': 'data/train.gzip',
     'val_path': 'data/validation.gzip',
