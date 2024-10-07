6f8060035d8e760b270c86c5dad84ed77e2cf5fa
dania-tii <dania.herzalla@tii.ae>
2024-10-07 17:03:32 +0400

    Update config

--- config.py
+++ config.py
@@ -9,22 +9,23 @@ params = {
     'hidden_channels': 256,
     'out_channels': 32,
     'out_features': 2,  # 3 jammer pos (x, y, z) # 5 (r, sin(theta), cos(theta), sin(phi), cos(phi))
-    'max_epochs': 5,
+    'max_epochs': 200,
     '3d': False,
     'coords': 'cartesian',  # opts: 'polar', 'cartesian'
     'required_features': ['node_positions', 'node_noise', 'timestamps', 'angle_of_arrival'],  # node_positions, polar_coordinates, node_noise, node_rssi
-    'additional_features': ['mean_noise', 'std_noise', 'range_noise', 'dist_to_centroid', 'sin_azimuth', 'cos_azimuth'],  # ['sin_azimuth', 'cos_azimuth', 'relative_noise', 'proximity_count', 'std_noise', 'range_noise'],
+    'additional_features': ['mean_noise', 'std_noise', 'range_noise', 'dist_to_centroid', 'sin_azimuth', 'cos_azimuth', 'moving_avg_aoa'],  # ['sin_azimuth', 'cos_azimuth', 'relative_noise', 'proximity_count', 'std_noise', 'range_noise'],
     'num_neighbors': 50,
     'edges': 'knn',  # opts: 'knn', 'proximity'
     'norm': 'minmax',  # opts: 'minmax', 'unit_sphere'
     'activation': False,
-    'experiments_folder': 'log_new/dynamic_path/test/',
-    'dataset_path': 'data/train_test_data/log_distance/guided_path_data.csv',  # combined_fspl_log_distance.csv',  # combined_urban_area.csv
-    'test_sets': ['test_dataset.pt', 'circle_test_set.pt', 'triangle_test_set.pt', 'rectangle_test_set.pt', 'random_test_set.pt', 'circle_jammer_outside_region_test_set.pt', 'triangle_jammer_outside_region_test_set.pt', 'rectangle_jammer_outside_region_test_set.pt', 'random_jammer_outside_region_test_set.pt', 'all_jammed_test_set.pt', 'all_jammed_jammer_outside_region_test_set.pt'],
+    'max_nodes': 60,
+    'experiments_folder': 'dynamic/controlled/',
+    'dataset_path': 'data/guided_path_data.csv',  # combined_fspl_log_distance.csv',  # combined_urban_area.csv
+    'test_sets': ['raw_test_data.csv'],
+    'dynamic': True,
     'train_per_class': False,
     'all_env_data': False,
     'inference': False,
-    'save_data': True,
     'reproduce': True,
     'plot_network': False,
     'study': 'dataset'  # dataset, coord_system, feat_engineering, knn_edges
@@ -85,3 +86,4 @@ params = {
     # 'num_layers': 2,
     # 'hidden_channels': 512,
     # 'out_channels': 256,
+
